5d5f58a87411c75bacecbb85d499cc8e0409f727
phillmac <phillmac@users.noreply.github.com>
2019-02-17 13:32:34 +1100

    Fix setup name

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ from dagr_revamped import __version__
 from setuptools import setup, find_packages
 
 setup(
-    name='Dagr Revamped',
+    name='dagr_revamped',
     version=__version__,
     description='A deviantArt image downloader script written in Python',
     author='Phillip Mackintosh',
     url='https://github.com/phillmac/dagr',
     packages=find_packages(),
-    install_requires=["MechanicalSoup >= 0.10.0"],
+    install_requires=[
+        'MechanicalSoup >= 0.10.0',
+        'docopt >=0.6.2'
+        ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     entry_points={
         'console_scripts': ['dagr.py=dagr_revamped.__main__:main']
