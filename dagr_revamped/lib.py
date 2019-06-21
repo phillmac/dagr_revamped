ca951f0f33a6533ac0510964f63ceeee178eb0cf
phillmac <phillmac@users.noreply.github.com>
2019-06-21 12:18:22 +1000

    Fix invalid suffix

--- dagr_revamped/lib.py
+++ dagr_revamped/lib.py
@@ -742,7 +742,7 @@ class DeviantionProcessor():
                 .joinpath(self.__findlink_debug_loc)
                 .joinpath(re.sub('[^a-zA-Z0-9_-]+', '_', shorten_url(self.page_link)))
                 .expanduser().resolve()
-                .with_suffix('html'))
+                .with_suffix('.html'))
             self.__logger.info('Dumping html to {}'.format(debug_output))
             debug_output.write_bytes(resp.content)
         raise DagrException('all attemps to find a link failed')
