019bad245e3abd96ddc45a08a7414dab79590e37
phillmac <phillmac@users.noreply.github.com>
2020-08-12 11:02:04 +1000

    Fix find flash video plugin

--- dagr_revamped/builtin_plugins/findlink_flash_video.py
+++ dagr_revamped/builtin_plugins/findlink_flash_video.py
@@ -7,12 +7,13 @@ def setup(manager):
 
 
 def find_flash_video(browser):
-    #browser = pickle.loads(browser)
     current_page = browser.get_current_page()
-    iframe_search = current_page.find('iframe', {'class': 'flashtime'})
-    if iframe_search:
-        browser.open(iframe_search.attrs.get('src'))
-        current_page = browser.get_current_page()
-        embed_search = current_page.find('embed', {'id': 'sandboxembed'})
-        if embed_search:
-            return embed_search.attrs.get('src')
+    stage = current_page.find('div', {'data-hook': 'art_stage'})
+    if stage:
+        iframe_search = stage.find('iframe')
+        if iframe_search:
+            browser.open(iframe_search.attrs.get('src'))
+            current_page = browser.get_current_page()
+            embed_search = current_page.find('embed', {'id': 'sandboxembed'})
+            if embed_search:
+                return embed_search.attrs.get('src')
