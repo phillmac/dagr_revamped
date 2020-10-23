2894c337f0b6baf05efb424162e9835b70591935
phillmac <phillmac@users.noreply.github.com>
2020-10-23 12:42:37 +1100

    Handle albums and collections

--- dagr_revamped/builtin_plugins/classes/SeleniumCrawler.py
+++ dagr_revamped/builtin_plugins/classes/SeleniumCrawler.py
@@ -1,9 +1,11 @@
 
 
 import logging
+from pathlib import PurePosixPath
 from time import sleep, time
 
 from selenium.webdriver.common.keys import Keys
+
 
 class SeleniumCrawler():
     def __init__(self, app_config, config, browser, cache):
@@ -12,7 +14,8 @@ class SeleniumCrawler():
         self.__browser = browser
         self.__cache = cache
         self.__oom_max_pages = self.__config.get('oom_max_pages', 13000)
-        self.__logger.log(level=15, msg=f"OOM max pages set to {self.__oom_max_pages}")
+        self.__logger.log(
+            level=15, msg=f"OOM max pages set to {self.__oom_max_pages}")
 
     def collect_pages(self):
         collect_st = time()
@@ -20,33 +23,65 @@ class SeleniumCrawler():
         try:
             pages.update(self.__browser.execute_async_script(
                 """
-        const done = arguments[0];
-        const links = document.querySelectorAll("a[data-hook=deviation_link]")
-        const pages = new Set()
-        for (const l of links) {
-            pages.add(l.getAttribute("href"))
-        }
-        done([...pages])
-        """
-            ))
+const done = arguments[0];
+const links = document.querySelectorAll("a[data-hook=deviation_link]")
+const pages = new Set()
+for (const l of links) {
+  pages.add(l.getAttribute("href"))
+}
+done([...pages])
+        """))
         except:
             self.__logger.exception('Error while collecting pages')
         self.__logger.log(
             level=15, msg=f"Collect pages took {'{:.4f}'.format(time() - collect_st)} seconds")
         return pages
 
-    def crawl_action(self, slug, pages=None, history=None):
+    def collect_pages_mval_id(self, mval_id):
+        collect_st = time()
+        pages = set()
+        try:
+            pages.update(self.__browser.execute_async_script(
+                """
+const collect_links = async (mvalID) => {
+  if (mvalID) {
+    const mvalDiv = document.getElementById(mvalID)
+    if (!mvalDiv) { throw new Error(`Element with id ${mvalID} not found.`) }
+    const done = arguments[arguments.length - 1]
+    const links = mvalDiv.querySelectorAll("a[data-hook=deviation_link]")
+    const pages = new Set()
+    for (const l of links) {
+      pages.add(l.getAttribute("href"))
+    }
+    done([...pages])
+  } else {
+    throw new Error('Missing required mvalID param')
+  }
+}
+collect_links(arguments[0])
+        """, mval_id))
+        except:
+            self.__logger.exception('Error while collecting pages')
+        self.__logger.log(
+            level=15, msg=f"Collect pages took {'{:.4f}'.format(time() - collect_st)} seconds")
+        return pages
+
+    def crawl_action(self, slug, mval_id=None, pages=None, history=None):
         pcount = None
         sleep_time = 0
-        if pages is None: pages = set()
-        if isinstance(pages, list): pages = set(pages)
-        if history is None: history = set()
+        if pages is None:
+            pages = set()
+        if isinstance(pages, list):
+            pages = set(pages)
+        if history is None:
+            history = set()
         body = self.__browser.find_element_by_tag_name('body')
         while (pcount is None) or (pcount < len(pages)):
             pcount = len(pages)
             for _pd in range(self.__config.get('page_down_count', 7)):
                 crawl_st = time()
-                collected = self.collect_pages()
+                collected = self.collect_pages_mval_id(
+                    mval_id) if mval_id else self.collect_pages()
                 if len(collected - pages) > 0:
                     sleep_time = self.__config.get('page_sleep_time', 15)
                     pages.update(collected)
@@ -57,8 +92,10 @@ class SeleniumCrawler():
                 try:
                     body.send_keys(Keys.PAGE_DOWN)
                 except:
-                    self.__logger.exception('Error while sending page down keypress')
-                self.__logger.log(level=15, msg=f"Sending page down keypress took {'{:.4f}'.format(time() - pd_st)} seconds")
+                    self.__logger.exception(
+                        'Error while sending page down keypress')
+                self.__logger.log(
+                    level=15, msg=f"Sending page down keypress took {'{:.4f}'.format(time() - pd_st)} seconds")
                 new_pages = pages - history
                 if len(new_pages) > 0:
                     hlen = len(history)
@@ -74,13 +111,30 @@ class SeleniumCrawler():
                     sleep(1)
                 self.__logger.log(
                     level=15, msg=f"Crawl took {'{:.4f}'.format(time() - crawl_st)} seconds")
-            if len(pages) > self.__oom_max_pages: break # Prevent chrome from crashing with 'Out of Memory'
+            if len(pages) > self.__oom_max_pages:
+                break  # Prevent chrome from crashing with 'Out of Memory'
         self.__cache.flush(slug)
         return pages
 
     def crawl(self, url_fmt, mode, deviant, mval=None, msg=None, full_crawl=False):
-        full_crawl = full_crawl or self.__config.get('full_crawl', '').lower() == 'force'
-        slug = f"{deviant}_{mode}"
+        full_crawl = full_crawl or self.__config.get(
+            'full_crawl', '').lower() == 'force'
+        slug = None
+        mval_id = None
+        deviant_lower = deviant.lower()
+        url = {
+            'gallery': f"https://www.deviantart.com/{deviant_lower}/gallery/all",
+            'favs': f"https://www.deviantart.com/{deviant_lower}/favourites/all",
+            'album': f"https://www.deviantart.com/{deviant_lower}/gallery/{mval}",
+            'collection': f"https://www.deviantart.com/{deviant_lower}/favourites/{mval}"
+        }.get(mode)
+
+        if mval:
+            mval_path = PurePosixPath(mval)
+            slug = f"{deviant}_{mode}_{'_'.join(mval_path.parts)}"
+            mval_id = mval_path.parent.name
+        else:
+            slug = f"{deviant}_{mode}"
         pages = set()
         history = set()
         history.update(self.__cache.query(slug))
@@ -88,10 +142,6 @@ class SeleniumCrawler():
             pages.update(history)
         else:
             self.__logger.info('Performing full crawl, no history loaded')
-        url = {
-            'gallery': f"https://www.deviantart.com/{deviant}/gallery/all",
-            'favs': f"https://www.deviantart.com/{deviant}/favourites/all"
-            }.get(mode)
 
         self.__browser.open_do_login(url)
-        return self.crawl_action(slug, pages, history)
+        return self.crawl_action(slug, mval_id, pages=pages, history=history)
