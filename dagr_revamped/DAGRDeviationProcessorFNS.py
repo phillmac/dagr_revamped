b5fad3278e498e4047d8db2981b502c753d538bc
phillmac <phillmac@users.noreply.github.com>
2020-12-17 12:55:30 +1100

    Fix init typo

--- dagr_revamped/DAGRDeviationProcessorFNS.py
+++ dagr_revamped/DAGRDeviationProcessorFNS.py
@@ -5,7 +5,7 @@ import requests
 class DAGRDeviationProcessorFNS(DAGRDeviationProcessor):
 
     def __init__(self, ripper, cache, page_link, **kwargs):
-        super().__init__(self, ripper, cache, page_link, **kwargs)
+        super().__init__(ripper, cache, page_link, **kwargs)
         self.fns_address = self.config.get(
             'dagr.deviationprocessor', 'fns_address')
         if self.fns_address is None or self.fns_address == '':
