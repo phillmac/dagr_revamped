b957b859053509f8b50e3ad50fc3139f677d9ce5
phillmac <phillmackintosh@gmail.com>
2021-06-28 10:02:13 +0000

    Use lit text h2

--- dagr_revamped/lib.py
+++ dagr_revamped/lib.py
@@ -1036,11 +1036,17 @@ class DAGRDeviationProcessor():
             self.__logger.log(level=5, msg='Found literature')
             self.__file_link, self.__found_type = current_url, 'literature'
             return self.__file_link, self.__found_type
-        lit_class = current_page.find('div', {'class': '_2JHSA'})
-        if lit_class and lit_class.text.lower() == 'literature':
+        lit_h2 = current_page.find('h2', string='Literature Text')
+        if lit_h2:
             self.__logger.log(level=5, msg='Found eclipse literature')
             self.__file_link, self.__found_type = current_url, 'literature'
             return self.__file_link, self.__found_type
+
+        # lit_class = current_page.find('div', {'class': '_2JHSA'})
+        # if lit_class and lit_class.text.lower() == 'literature':
+        #     self.__logger.log(level=5, msg='Found eclipse literature')
+        #     self.__file_link, self.__found_type = current_url, 'literature'
+        #     return self.__file_link, self.__found_type
         journal = current_page.find('div', {'class': 'journal-wrapper'})
         if 'journal' in current_url or journal:
             self.__logger.log(level=5, msg='Found journal')
