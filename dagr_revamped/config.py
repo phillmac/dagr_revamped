ff01f583971982f877284cdb2279fef0e2ebf3b5
phillmac <phillmackintosh@gmail.com>
2021-06-30 23:41:27 +0000

    Add queurman_url option

--- dagr_revamped/config.py
+++ dagr_revamped/config.py
@@ -347,7 +347,7 @@ class DAGRConfig(DAGRBaseConf):
         'Dagr.Plugins.Classes': get_os_options('Dagr.Plugins.Classes', ['Browser', 'Resolver', 'Crawler', 'Processor', 'Crawler_Cache']),
         'Dagr.Plugins.Selenium': get_os_options('Dagr.Plugins.Selenium', [
             'Enabled', 'Webdriver_Mode', 'Webdriver_URL', 'Webdriver_Max_Tries' 'Driver_Path', 'Full_Crawl', 'Login_Policy', 'OOM_Max_Pages',
-            'Local_Cache_Path', 'Remote_Cache_Path', 'Remote_Cache_Type', 'Remote_Breaker_Fail_Max', 'Remote_Breaker_Reset_Timeout', 'Unload_Cache_Policy'
+            'Local_Cache_Path', 'Remote_Cache_Path', 'Remote_Cache_Type', 'Remote_Breaker_Fail_Max', 'Remote_Breaker_Reset_Timeout', 'Unload_Cache_Policy', 'QueueMan_Url'
         ]),
         'DeviantArt': get_os_options('DeviantArt', ['Username', 'Password'])
     })
