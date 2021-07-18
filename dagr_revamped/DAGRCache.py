5a640e56c8b1dfd3ab2d387c48befa908d8e1444
phillmac <phillmac@users.noreply.github.com>
2021-07-18 14:13:20 +1000

    Fix errors_404 list

--- dagr_revamped/DAGRCache.py
+++ dagr_revamped/DAGRCache.py
@@ -504,7 +504,7 @@ class DAGRCache():
         result.update((u.lower() for u in self.__premium))
         if self.__httperrors is None:
             self.__httperrors = self.__load_httperrors()
-        errors_404 = [k for k,v in self.__httperrors.items() if v['error_code'] == 404]
+        errors_404 = [k for k,v in self.__httperrors.items() if any(e['error_code'] == 404 for e in v)]
         result.update((u.lower() for u in errors_404))
         return result
 
